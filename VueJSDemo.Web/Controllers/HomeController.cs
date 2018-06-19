5ce26f55a36a6009c2280fd85d664a9de9c05cda
Blimie Kohn <blimiekohn@gmail.com>
2018-06-18 22:52:54 -0400

    initial commit

---
+++ VueJSDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VueJSDemo.Data;
+
+namespace VueJSDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult People()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            return Json(repo.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public void AddPerson(Person person)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Add(person);
+        }
+
+        [HttpPost]
+        public void Update(Person person)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Update(person);
+        }
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Delete(id);
+        }
+    }
+
+}
---
+++ VueJSDemo.Web/Scripts/index.js
@@ -0,0 +1,73 @@
+﻿new Vue({
+    el: '#app',
+    mounted: function () {
+        this.loadPeople();
+    },
+    data: {
+        people: [],
+        modalPerson: {
+            FirstName: '',
+            LastName: '',
+            Age: ''
+        },
+        isEditMode: false,
+        sortAsc: true
+    },
+    methods: {
+        loadPeople: function (cb) {
+            $.get('/home/getall', people => {
+                this.people = people;
+                if (cb) {
+                    cb();
+                }
+            });
+        },
+        newClick: function () {
+            $(".modal").modal();
+            this.isEditMode = false;
+        },
+
+        addClick: function () {
+            $.post('/home/addperson', this.modalPerson, () => {
+                this.loadPeople(() => {
+                    $('.modal').modal('hide');
+                    this.modalPerson = {
+                        FirstName: '',
+                        LastName: '',
+                        Age: ''
+                    }
+                });
+            });
+        },
+
+        editClick: function (person) {
+            this.isEditMode = true;
+            this.modalPerson = Object.assign({}, person);
+            $(".modal").modal();
+        },
+
+        updateClick: function () {
+            $.post('/home/update', this.modalPerson, () => {
+                this.loadPeople(() => {
+                    $('.modal').modal('hide');
+                    this.modalPerson = {
+                        FirstName: '',
+                        LastName: '',
+                        Age: ''
+                    }
+                });
+            });
+        }, 
+        deleteClick: function (id) {
+            $.post('/home/delete', { id }, () => {
+                this.loadPeople();
+            });
+        },
+        sortClick: function () {
+            let ascDesc = this.sortAsc ? 1 : -1;
+            this.people.sort((a, b) => a.Age - b.Age * ascDesc);
+
+            this.sortAsc = !this.sortAsc;
+        }
+    }
+});
